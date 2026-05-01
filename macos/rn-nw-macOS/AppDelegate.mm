57a60c53515a780a39db5386ef86c7525b65e149
Rudolf Bok <78938158+rudolfbok@users.noreply.github.com>
2026-05-01 23:54:22 +0200

    fix: renaming

--- macos/rn-nw-macOS/AppDelegate.mm
+++ macos/rn-nw-macOS/AppDelegate.mm
@@ -7,7 +7,7 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-  self.moduleName = @"macos";
+  self.moduleName = @"rn-nw";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
