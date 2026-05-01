ceb67258180b153777e9199dd90e04b6ad5237c6
Rudolf Bok <78938158+rudolfbok@users.noreply.github.com>
2026-05-01 22:10:27 +0200

    feature: starting screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,41 @@
-import { useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import { FileTsIcon, IconProps, LaptopIcon, LightbulbIcon, PackageIcon, WindIcon } from 'phosphor-react-native';
+import { ComponentType } from 'react';
+import { Text, View, useColorScheme } from 'react-native';
 import './globals.css';
 
+const FeaturePill = ({ Icon, name, color }: { Icon: ComponentType<IconProps>; name: string; color: string }) => {
+	return (
+		<View className="flex-row items-center gap-2 rounded-3xl border border-[#909090] px-4 py-2 dark:border-[#2A2C32]">
+			<Icon color={color} />
+			<Text style={{ color }}>{name}</Text>
+		</View>
+	);
+};
+
 export default function App() {
-	const [count, setCount] = useState(0);
+	const currentTheme = useColorScheme();
+
+	const switchColor = (lightColor: string, darkColor: string): string => {
+		const color = currentTheme === 'light' ? lightColor : darkColor;
+		return color;
+	};
 
 	return (
-		<View className="flex-1 items-center justify-center">
-			<View>
-				<Text style={{ fontSize: 24 }} className="text-red-400">
-					React Native macOS with Nativewind
-				</Text>
+		<View className="flex-1 items-center justify-center bg-[#F2F2F7] dark:bg-[#0D1119]">
+			<View className="items-center gap-4 rounded-3xl bg-white p-10 dark:bg-[#161A21]">
+				<Text className="text-5xl font-bold">React Native macOS</Text>
+				<Text>A clean starter template for building macOS apps with React Native, TypeScript, and Nativewind.</Text>
+				<View className="flex-row gap-2">
+					<FeaturePill Icon={LaptopIcon} name="React Native macOS" color={switchColor('black', 'white')} />
+					<FeaturePill Icon={WindIcon} name="Nativewind" color="#05900A" />
+					<FeaturePill Icon={FileTsIcon} name="TypeScript" color="#0079FF" />
+					<FeaturePill Icon={PackageIcon} name="PNPM" color="#EB9331" />
+				</View>
+				<View className="flex-row items-center gap-2">
+					<LightbulbIcon color={switchColor('black', 'white')} />
+					<Text>Edit App.tsx to start building your app</Text>
+				</View>
 			</View>
-			<Text style={{ fontSize: 30 }} className="text-blue-400">
-				Count: {count}
-			</Text>
-			<Button title="Set count" onPress={() => setCount(count + 1)} />
 		</View>
 	);
 }
